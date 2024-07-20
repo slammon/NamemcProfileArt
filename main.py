b48da5c3b2ef5c6e148d5f20c5ad6839d2b99ab8
soupbit <56459644+G502045@users.noreply.github.com>
2024-07-19 21:01:18 -0400

    resize image

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ import time
 try:
     from PIL import Image
 except ImportError:
-    print("Pillow library not found. Installing now...")
+    print("[Err]: Pillow library not found. Installing now...")
     try:
         subprocess.check_call(["python", "-m", "pip", "install", "pillow"])
         from PIL import Image
     except subprocess.CalledProcessError as e:
-        print(f"Failed to install Pillow: {e}")
+        print(f"[Err]: Failed to install Pillow: {e}")
         exit(1)
 
-input_image = Image.open('assets/input.png')
+input_image = Image.open(input('[Input]: Path to input image: '))
 skin_template = Image.open('assets/skin_template.png')
 
 if input_image.size != (72, 24):
-    raise ValueError('Input image must be 72x24 pixels')
+    print('[Err]: Input image must be 72x24 pixels\n[Log]: Attempting to resize image')
+    try:
+        input_image = input_image.resize((72, 24))
+        print('[Log]: Image resized successfully')
+    except Exception as e:
+        print(f'[Err]: Failed to resize image: {e}')
+        exit(1)
 
 os.makedirs('assets/output', exist_ok=True)
-os.makedirs('assets/output2', exist_ok=True)
 
 counter = 26
 
@@ -31,9 +36,7 @@ for i in range(3):
         if i == 0 and j == 0:
             continue
         block = input_image.crop((j*8, i*8, j*8+8, i*8+8))
-        
-        block.save(f'assets/output2/block_{counter}.png')
-        
+
         skin = skin_template.copy()
         skin.paste(block, (8, 8))
         
